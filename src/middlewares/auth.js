d05f604508c4070256117c5ce104cc84a6e5fbd2
lucasbsand <124009743+lucasbsand@users.noreply.github.com>
2023-11-13 02:35:52 -0300

    Change

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,16 +1,20 @@
-const dotenv = require('dotenv');
-const mongoose = require('mongoose');
-const jwt = require('jsonwebtoken');
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
 
 dotenv.config();
 
 function generateToken(userId) {
-    if (typeof userId !== 'string') return jwt.sign({'_id': JSON.stringify(userId)}, process.env.TOKEN, { expiresIn: '7d'});
-    else return jwt.sign({'_id': userId}, process.env.TOKEN, { expiresIn: '7d'})
+  if (typeof userId !== "string")
+    return jwt.sign({ _id: JSON.stringify(userId) }, process.env.TOKEN, {
+      expiresIn: "7d",
+    });
+  else return jwt.sign({ _id: userId }, process.env.TOKEN, { expiresIn: "7d" });
 }
 
 function userFromToken(token) {
-    return mongoose.Types.ObjectId(jwt.decode(token, process.env.TOKEN));
-};
+  console.log(token);
+  //   return mongoose.Types.ObjectId(jwt.decode(token, process.env.TOKEN));
+}
 
 module.exports = { generateToken, userFromToken };
